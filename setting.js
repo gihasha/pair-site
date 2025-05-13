7f29bbf50a61b594693a6d3176083432968dccac
gihasha <dasunfree5@gmail.com>
2025-05-13 08:22:43 +0530

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,12 @@
+module.exports = {
+    PREFIX: "DASUN-MAX [KILL]>>>",
+    SUCCESS_NUMBERS: ["94765775550"], 
+    SUCCESS_MESSAGE: "*🌝DASUN MAX PAIR SITE OWNER 🚀*",
+    CONNECTED_MESSAGE: `
+┏━━━━━━━━━━━━━━
+┃📍 *DASUN MAX SESSION ID*
+┃📍 *SUCCESSFULLY*
+┃📍 *CONNECTED ✅🔥*
+┗━━━━━━━━━━━━━━━
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬`
+};
